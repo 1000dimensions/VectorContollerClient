03ae0f319f969ee6e41c5bfc37b6a1583360af3a
Jakson Vermillion <1000dimensions@github.com>
2024-08-02 14:42:00 -0400

    added some controls

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,20 @@ use termion::raw::IntoRawMode;
 fn main() {
     keyboardCap();
 }
-fn robot(xy: [f32; 2]) {
+fn robot(xy: [f32; 2], spin: f32, edfbrr: f32, lock: bool) {
     println!("{:?}", xy);
-    
+    println!("{:?}", spin);
+    println!("{:?}", edfbrr);
+    println!("{:?}", lock);
+
 
 }
 fn keyboardCap() {
     let mut speed: f32 = 0.0;
     let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
-    
+    let mut swervelock = false;
+    let mut edfenable = false;
    
     write!(
         stdout,
@@ -32,6 +36,12 @@ fn keyboardCap() {
         let mut xy : [f32; 2] = [0.0; 2];
         let mut x = 0.0;
         let mut y = 0.0;
+        let mut spin = 0.0;
+        let mut edfbrr = 0.0;
+        if edfenable == true {
+            edfbrr = 0.5;
+        }
+         
         write!(
             stdout,
             "{}{}",
@@ -39,6 +49,7 @@ fn keyboardCap() {
             termion::clear::CurrentLine
         )
         .unwrap();
+
         
         match k.as_ref().unwrap() {
             Key::Char('a') => x = -1.0,
@@ -50,6 +61,15 @@ fn keyboardCap() {
             Key::Char('2') => speed = 0.25,
             Key::Char('3') => speed = 0.5,
             Key::Char('4') => speed = 0.9,
+            Key::Char('q') => spin = -1.0,
+            Key::Char('e') => spin = 1.0,
+            Key::Char('v') => {swervelock = !swervelock;}
+            Key::Char('f') => {edfenable = !edfenable;}
+            Key::Char(' ') => {
+                if edfenable == true{
+                    edfbrr = 1.0
+                }
+            }
             _ => {
                 x = 0.0;
                 y = 0.0;
@@ -60,7 +80,8 @@ fn keyboardCap() {
         y = y * speed;
         xy[0] = x;
         xy[1] = y;
-        robot(xy)
+        spin = spin * speed;
+        robot(xy, spin, edfbrr, swervelock);
     }
         
     write!(stdout, "{}", termion::cursor::Show).unwrap();
