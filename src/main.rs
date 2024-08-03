378dd8c893fa3507b9e79aeed1fdfc170d17623f
Jakson Vermillion <1000dimensions@github.com>
2024-08-03 18:27:22 -0400

    packaging complete

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,38 @@ fn main() {
     keyboardCap();
 }
 //robot caputures all flags and then assembles them into a nice array. 
-fn robot(xy: [f32; 2], spin: f32, edfbrr: f32, lock: bool, shutoff: bool) {
-    println!("{:?}", xy);
-    println!("{:?}", spin);
-    println!("{:?}", edfbrr);
-    println!("{:?}", lock);
-    println!("{:?}", shutoff);
-
+fn robot(g: f32, y: f32, spin: f32, edfbrr: u16, lock: bool, shutoff: bool) {
+    let mut bytes = g.to_le_bytes();
+    let mut package: [u8; 16] = [0; 16];
+    let mut position:usize = 0;
+    for x in bytes{
+        package[position] = x;
+        position += 1
+    }
+    position = 4; 
+    bytes = y.to_le_bytes();
+    for x in bytes{
+        package[position] = x;
+        position += 1
+    }
+    position = 8;
+    bytes = spin.to_le_bytes();
+    for x in bytes{
+        package[position] = x;
+        position += 1
+    }
+    position = 12;
+    let byte = edfbrr.to_le_bytes();
+    for x in byte{
+        package[position] = x;
+        position += 1
+    }
+    let mut bit = 0;
+    bit |= (lock as u8) << 0;
+    bit |= (shutoff as u8) << 1;
+    position = 14;
+    package[position] = bit;
+    println!{"{:?}", package};
 
 }
 fn keyboardCap() {
@@ -35,13 +60,12 @@ fn keyboardCap() {
     
     let mut speed:f32 = 0.0;
     for k in stdin.keys() {
-        let mut xy : [f32; 2] = [0.0; 2];
         let mut x = 0.0;
         let mut y = 0.0;
         let mut spin = 0.0;
-        let mut edfbrr = 0.0;
+        let mut edfbrr:u16 = 0;
         if edfenable == true {
-            edfbrr = 0.05;
+            edfbrr = 3277;
         }
          
 
@@ -60,10 +84,10 @@ fn keyboardCap() {
             Key::Char('e') => spin = 1.0,
             Key::Char('v') => {swervelock = !swervelock;}
             Key::Char('f') => {edfenable = !edfenable;}
-            Key::Char('o') => emergancy = true;
+            Key::Char('o') => {emergancy = true;},
             Key::Char(' ') => {
                 if edfenable == true{
-                    edfbrr = 1.0
+                    edfbrr = 65535;
                 }
             }
             _ => {
@@ -73,10 +97,8 @@ fn keyboardCap() {
         }
         x = x * speed;
         y = y * speed;
-        xy[0] = x;
-        xy[1] = y;
         spin = spin * speed;
-        robot(xy, spin, edfbrr, swervelock, emergancy);
+        robot(x, y, spin, edfbrr, swervelock, emergancy);
         // test functions to see if edf goes brrrrr.
         //println!("is edf going brr? {:?}", edfenable);
         //println!("how fast is edf going brr? {:?}", edfbrr);
